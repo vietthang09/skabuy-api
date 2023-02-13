76ecb07806828678e1ab8c2837d9655435fa52df
vietthang09 <vthcvn@gmail.com>
2023-02-13 09:27:44 +0700

    fix: enable cors

--- index.js
+++ index.js
@@ -3,16 +3,10 @@ const cors = require("cors");
 const bodyparser = require("body-parser");
 require("dotenv").config();
 
-const app = express();
+let app = express();
 app.use(cors());
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
+app.options('*', cors());
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 
 app.use("/shop", require("./Route/Shop.route"));
 app.use("/user", require("./Route/User.route"));
